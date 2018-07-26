6344ced08dc4a587b60e6ce92df5833f9e2b514f
John Gray <john.gray90@gmail.com>
2018-07-26 11:22:57 +0100

    lispy v.0.0.0.2: RePL (reads, prints, loops)

--- eval.c
+++ eval.c
@@ -5,7 +5,17 @@ static char input[2048];
 
 int main(int argc, char** argv){
 
-  puts("Welcome to Lispy v.0.0.0.1. Press Ctrl+c to quit.");
+  puts("Welcome to Lispy v.0.0.0.2. Press Ctrl+c to quit.");
+
+  while(1){
+
+    fputs("lispy> ", stdout);
+
+    fgets(input, 2048, stdin);
+
+    printf("echo: %s", input);
+
+  }
 
   return 0;
 
